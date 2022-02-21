073c08d373ed0dddfda26aa70f44bd5c5de681e5
Eddys99 <grama.eddy@yahoo.com>
2022-02-21 19:02:19 +0200

    get the task with the most assigned workers/tags

--- src/controllers/read.js
+++ src/controllers/read.js
@@ -6,7 +6,18 @@ async function tasks(req, res) {
         res.send(tasks);
     } catch(err) {
         console.log(err);
-        res.redirect("/read");
+        res.redirect("No tasks yet");
+    }
+}
+
+async function taskById(req, res) {
+    try {
+        let query = { _id: req.params.id };
+        const task = await TaskModel.findOne(query);
+        res.send(task);
+    } catch(err) {
+        console.log(err);
+        res.send("Task not found.");
     }
 }
 
@@ -32,8 +43,51 @@ async function unfinishedTasks(req, res) {
     }
 }
 
+async function availableTasks(req, res) {
+    try {
+        let query = { deleted: false };
+        const availableTasks = await TaskModel.find(query);
+        res.send(availableTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No available tasks.");
+    }
+}
+
+async function mostAssignedWorkers(req, res) {
+    try {
+        const mostAssignedWorkersTask = await TaskModel.aggregate([
+            { $unwind : "$responsibleWorkers" },
+            { $group : { _id : "$_id", len : { $sum : 1 } } },
+            { $sort : { len : -1 } },
+          ]);
+        res.redirect("/read/" + mostAssignedWorkersTask[0]._id);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks.");
+    }
+}
+
+async function mostAssignedTags(req, res) {
+    try {
+        const mostAssignetTags = await TaskModel.aggregate([
+            { $unwind : "$tags" },
+            { $group : { _id : "$_id", len : { $sum : 1 } } },
+            { $sort : { len : -1 } },
+          ]);
+        res.redirect("/read/" + mostAssignetTags[0]._id);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks.");
+    }
+}
+
 module.exports = {
     tasks,
+    taskById,
     completedTasks,
-    unfinishedTasks
+    unfinishedTasks,
+    availableTasks,
+    mostAssignedWorkers,
+    mostAssignedTags
 }
--- src/routes/read.js
+++ src/routes/read.js
@@ -4,8 +4,16 @@ const read = require("../controllers/read");
 
 router.get("/", read.tasks);
 
+router.get("/:id", read.taskById);
+
 router.get("/completed-tasks", read.completedTasks);
 
 router.get("/unfinished-tasks", read.unfinishedTasks);
+
+router.get("/available-tasks", read.availableTasks);
+
+router.get("/most-assigned-task/workers", read.mostAssignedWorkers);
+
+router.get("/most-assigned-task/tags", read.mostAssignedTags);
 
 module.exports = router;
