256aef28f545ee1e043994bbfcd59cd2dd970615
Eddys99 <grama.eddy@yahoo.com>
2022-02-22 18:29:40 +0200

    Read TODOs counters and Read sorted tasks by workers/tags asc/desc

--- src/controllers/read.js
+++ src/controllers/read.js
@@ -56,12 +56,9 @@ async function availableTasks(req, res) {
 
 async function mostAssignedWorkers(req, res) {
     try {
-        const mostAssignedWorkersTask = await TaskModel.aggregate([
-            { $unwind : "$responsibleWorkers" },
-            { $group : { _id : "$_id", len : { $sum : 1 } } },
-            { $sort : { len : -1 } },
-          ]);
-        res.redirect("/read/" + mostAssignedWorkersTask[0]._id);
+        let sortQuery = { countWorkers: -1};
+        const mostAssignedWorkersTask = await TaskModel.find().sort(sortQuery).limit(1);
+        res.send(mostAssignedWorkersTask);
     } catch(err) {
         console.log(err);
         res.send("No tasks.");
@@ -70,15 +67,166 @@ async function mostAssignedWorkers(req, res) {
 
 async function mostAssignedTags(req, res) {
     try {
-        const mostAssignetTags = await TaskModel.aggregate([
-            { $unwind : "$tags" },
-            { $group : { _id : "$_id", len : { $sum : 1 } } },
-            { $sort : { len : -1 } },
-          ]);
-        res.redirect("/read/" + mostAssignetTags[0]._id);
+        let sortQuery = { tagsCount: -1};
+        const mostAssignedWorkersTask = await TaskModel.find().sort(sortQuery).limit(1);
+        res.send(mostAssignedWorkersTask);
     } catch(err) {
         console.log(err);
         res.send("No tasks.");
+    }
+}
+
+async function oneWorkerTasks(req, res) {
+    try {
+        let query = { responsibleWorkers: { $size: 1 }};
+        const getOneWorkerTasks = await TaskModel.find(query);
+        res.send(getOneWorkerTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks found.");
+    }
+}
+
+async function multipleWorkersTasks(req, res) {
+    try {
+        let query = { 'responsibleWorkers.1': { $exists: true }};
+        const getMultipleWorkersTasks = await TaskModel.find(query);
+        res.send(getMultipleWorkersTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks found.");
+    }
+}
+
+async function oneTagTasks(req, res) {
+    try {
+        let query = { tags: { $size: 1 }};
+        const getOneTagTasks = await TaskModel.find(query);
+        res.send(getOneTagTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks found.");
+    }
+}
+
+async function multipleTagTasks(req, res) {
+    try {
+        let query = { 'tags.1': { $exists: true }};
+        const getMultipleTagsTasks = await TaskModel.find(query);
+        res.send(getMultipleTagsTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks found.");
+    }
+}
+
+async function countCompletedTasks(req, res) {
+    try {
+        let filter = { isDone: true };
+        const numberOfCompletedTasks = await TaskModel.where(filter).count();
+        res.send("Completed tasks: " + numberOfCompletedTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function countUnfinishedTasks(req, res) {
+    try {
+        let filter = { isDone: false };
+        const numberOfUnfinishedTasks = await TaskModel.where(filter).count();
+        res.send("Unfinished tasks: " + numberOfUnfinishedTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function countTasksWithWorkers(req, res) {
+    try {
+        let filter = { 'responsibleWorkers.0': { $exists: true }};
+        const numberOfTasksWithWorkers = await TaskModel.where(filter).count();
+        res.send("Tasks that have workers: " + numberOfTasksWithWorkers);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function countTasksWithoutWorkers(req, res) {
+    try {
+        let filter = { responsibleWorkers: { $size: 0 }};
+        const numberOfTasksWithoutWorkers = await TaskModel.where(filter).count();
+        res.send("Tasks that have no workers: " + numberOfTasksWithoutWorkers);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function countTasksWithTags(req, res) {
+    try {
+        let filter = { 'tags.0': { $exists: true }};
+        const numberOfTasksWithTags = await TaskModel.where(filter).count();
+        res.send("Tasks with tags: " + numberOfTasksWithTags);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function countTasksWithoutTags(req, res) {
+    try {
+        let filter = { tags: { $size: 0 }};
+        const numberOfTasksWithoutTags = await TaskModel.where(filter).count();
+        res.send("Tasks with tags: " + numberOfTasksWithoutTags);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function sortAscByNumberOfWorkers(req, res) {
+    try {
+        let query = { countWorkers: 1 };
+        const getSortedTasks = await TaskModel.find().sort(query);
+        res.send(getSortedTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function sortDescByNumberOfWorkers(req, res) {
+    try {
+        let query = { countWorkers: -1 };
+        const getSortedTasks = await TaskModel.find().sort(query);
+        res.send(getSortedTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function sortAscByNumberOfTags(req, res) {
+    try {
+        let query = { tagsCount: 1 };
+        const getSortedTasks = await TaskModel.find().sort(query);
+        res.send(getSortedTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
+    }
+}
+
+async function sortDescByNumberOfTags(req, res) {
+    try {
+        let query = { tagsCount: -1 };
+        const getSortedTasks = await TaskModel.find().sort(query);
+        res.send(getSortedTasks);
+    } catch(err) {
+        console.log(err);
+        res.send("No tasks yet.");
     }
 }
 
@@ -89,5 +237,19 @@ module.exports = {
     unfinishedTasks,
     availableTasks,
     mostAssignedWorkers,
-    mostAssignedTags
+    mostAssignedTags,
+    oneWorkerTasks,
+    multipleWorkersTasks,
+    oneTagTasks,
+    multipleTagTasks,
+    countCompletedTasks,
+    countUnfinishedTasks,
+    countTasksWithWorkers,
+    countTasksWithoutWorkers,
+    countTasksWithTags,
+    countTasksWithoutTags,
+    sortAscByNumberOfWorkers,
+    sortDescByNumberOfWorkers,
+    sortAscByNumberOfTags,
+    sortDescByNumberOfTags
 }
--- src/routes/read.js
+++ src/routes/read.js
@@ -4,7 +4,7 @@ const read = require("../controllers/read");
 
 router.get("/", read.tasks);
 
-router.get("/:id", read.taskById);
+router.get("/get-this-task/:id", read.taskById);
 
 router.get("/completed-tasks", read.completedTasks);
 
@@ -15,5 +15,33 @@ router.get("/available-tasks", read.availableTasks);
 router.get("/most-assigned-task/workers", read.mostAssignedWorkers);
 
 router.get("/most-assigned-task/tags", read.mostAssignedTags);
+
+router.get("/one-worker-tasks", read.oneWorkerTasks);
+
+router.get("/multiple-workers-tasks", read.multipleWorkersTasks);
+
+router.get("/one-tag-tasks", read.oneTagTasks);
+
+router.get("/multiple-tags-tasks", read.multipleTagTasks);
+
+router.get("/count/completed-tasks", read.countCompletedTasks);
+
+router.get("/count/unfinished-tasks", read.countUnfinishedTasks);
+
+router.get("/count/tasks-with-workers", read.countTasksWithWorkers);
+
+router.get("/count/tasks-without-workers", read.countTasksWithoutWorkers);
+
+router.get("/count/tasks-with-tags", read.countTasksWithTags);
+
+router.get("/count/tasks-without-tags", read.countTasksWithoutTags);
+
+router.get("/sorted/by/workers/asc", read.sortAscByNumberOfWorkers);
+
+router.get("/sorted/by/workers/desc", read.sortDescByNumberOfWorkers);
+
+router.get("/sorted/by/tags/asc", read.sortAscByNumberOfTags);
+
+router.get("/sorted/by/tags/desc", read.sortDescByNumberOfTags);
 
 module.exports = router;
